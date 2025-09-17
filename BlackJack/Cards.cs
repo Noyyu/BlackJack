c57b221b5709266e55cca40917c437abcfe373bc
nikki norberg <nikki.norberg@hotmail.com>
2025-09-17 16:02:33 +0200

    added functionallity

---
+++ BlackJack/Cards.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackJack
+{
+    internal class Cards
+    {
+        private int totalValue = 0;
+        private int currentCard = 0;
+        private string name = "unnamed";
+
+        private bool lost = false;
+        private bool won = false;
+
+
+
+        public Cards(string playerName)
+        {
+            this.totalValue = 0;
+            this.currentCard = 0;
+            this.name = playerName;
+        }
+         public bool CheckStatAgainstThis(int playerSum)
+        {
+            if (this.totalValue > playerSum && this.totalValue <= 21)
+            {
+                return true;
+            }
+            return false;
+        }
+        public int CheckStat()
+        {
+            if (this.totalValue > 21)
+            {
+                this.lost = true;
+                return 1;
+            }
+            else if (this.totalValue == 21)
+            {
+                this.won = true;
+                return 2;
+            }
+            return -1;
+        }
+        public int GiveNewCard()
+        {
+            int cardValue = new Random().Next(1, 14); //1-13
+
+            totalValue += cardValue;
+            currentCard = cardValue;
+
+            return currentCard;
+        }
+        public int GetCardValue()
+        {
+            return this.currentCard;
+        }
+        public string GetName()
+        {
+            return this.name;
+        }
+        public void GetCurrentCardGraphic()
+        {
+            CardGraphic(currentCard);
+        }
+        public int GetTotalValue()
+        {
+            return this.totalValue;
+        }
+        private static void CardGraphic(int card)
+        {
+            if (card == 1)
+            {
+                Console.WriteLine("_______");
+                Console.WriteLine("|A .  |");
+                Console.WriteLine("| /.\\ |");
+                Console.WriteLine("|(_._)|");
+                Console.WriteLine("|  |  |");
+                Console.WriteLine("|____V|");
+            }
+            else if (card == 2)
+            {
+                Console.WriteLine("_______");
+                Console.WriteLine("|2    |");
+                Console.WriteLine("|  o  |");
+                Console.WriteLine("|     |");
+                Console.WriteLine("|  o  |");
+                Console.WriteLine("|____Z|");
+            }
+            else if (card == 3)
+            {
+                Console.WriteLine("_______");
+                Console.WriteLine("|3    |");
+                Console.WriteLine("|  o  |");
+                Console.WriteLine("|  o  |");
+                Console.WriteLine("|  o  |");
+                Console.WriteLine("|____E|");
+            }
+            else if (card == 4)
+            {
+                Console.WriteLine("_______");
+                Console.WriteLine("|4    |");
+                Console.WriteLine("| o o |");
+                Console.WriteLine("|     |");
+                Console.WriteLine("| o o |");
+                Console.WriteLine("|____h|");
+            }
+            else if (card == 5)
+            {
+                Console.WriteLine("_______");
+                Console.WriteLine("|5    |");
+                Console.WriteLine("| o o |");
+                Console.WriteLine("|  o  |");
+                Console.WriteLine("| o o |");
+                Console.WriteLine("|____S|");
+            }
+            else if (card == 6)
+            {
+                Console.WriteLine("_______");
+                Console.WriteLine("|6    |");
+                Console.WriteLine("| o o |");
+                Console.WriteLine("| o o |");
+                Console.WriteLine("| o o |");
+                Console.WriteLine("|____9|");
+            }
+            else if (card == 7)
+            {
+                Console.WriteLine("_______");
+                Console.WriteLine("|7    |");
+                Console.WriteLine("| o o |");
+                Console.WriteLine("|o o o|");
+                Console.WriteLine("| o o |");
+                Console.WriteLine("|____L|");
+            }
+            else if (card == 8)
+            {
+                Console.WriteLine("_______");
+                Console.WriteLine("|8    |");
+                Console.WriteLine("|o o o|");
+                Console.WriteLine("| o o |");
+                Console.WriteLine("|o o o|");
+                Console.WriteLine("|____8|");
+            }
+            else if (card == 9)
+            {
+                Console.WriteLine("_______");
+                Console.WriteLine("|9    |");
+                Console.WriteLine("|o o o|");
+                Console.WriteLine("|o o o|");
+                Console.WriteLine("|o o o|");
+                Console.WriteLine("|____6|");
+            }
+            else if (card == 10)
+            {
+                Console.WriteLine("_______");
+                Console.WriteLine("|10   |");
+                Console.WriteLine("|     |");
+                Console.WriteLine("|  X  |");
+                Console.WriteLine("|     |");
+                Console.WriteLine("|___0l|");
+            }
+            else if (card == 11)
+            {
+                Console.WriteLine("_______");
+                Console.WriteLine("|J    |");
+                Console.WriteLine("| /.\\ |");
+                Console.WriteLine("|(_._)|");
+                Console.WriteLine("|  |  |");
+                Console.WriteLine("|____[|");
+            }
+            else if (card == 12)
+            {
+                Console.WriteLine("_______");
+                Console.WriteLine("|Q    |");
+                Console.WriteLine("| /.\\ |");
+                Console.WriteLine("|(_._)|");
+                Console.WriteLine("|  |  |");
+                Console.WriteLine("|____O|");
+            }
+            else if (card == 13)
+            {
+                Console.WriteLine("_______");
+                Console.WriteLine("|K    |");
+                Console.WriteLine("| /.\\ |");
+                Console.WriteLine("|(_._)|");
+                Console.WriteLine("|  |  |");
+                Console.WriteLine("|____>|");
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ BlackJack/Program.cs
@@ -0,0 +1,170 @@
+﻿using System.Numerics;
+
+namespace BlackJack
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string line = "";
+            int timing = 30; //Standard timing
+            int currentCard = 0;
+            bool pass = false;
+            bool exit = false;
+            bool playerWon = false;
+
+            Cards dealer = new Cards("the Dealer");
+            Cards player = new Cards("the Player");
+
+            //Intro();
+
+            play(player);
+            play(dealer);
+
+            SpellItOut("Dealer: " + dealer.GetTotalValue() + ", Player: " + player.GetTotalValue(), timing);
+
+
+            while (exit == false)
+            {
+                if (pass == false) 
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkGray;
+                    Console.WriteLine("1: another card, 2. stay");
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+
+                if (YesOrNo() == true)
+                {
+                    pass = false;
+                }
+                else
+                {
+                    pass = true;
+                }
+
+                // Play
+                if (pass == false)
+                {
+                    play(player);
+                }
+                play(dealer);
+
+                SpellItOut("Dealer: " + dealer.GetTotalValue() + ", Player: " + player.GetTotalValue(), timing);
+                //---
+
+
+                // Win check
+                if (player.CheckStat() == 1 || dealer.CheckStatAgainstThis(player.GetTotalValue()) || dealer.CheckStat() == 2) //If the player has gotten a total value of more than 21 OR if the player have passed and the dealer got a higher total value than the player under 21 OR the dealer got 21..
+                {
+                    exit = true;
+                    //You lost
+                }
+                else if (player.CheckStat() == 2 || dealer.CheckStat() == 1) //If the player got 21 or if the dealer got more than 21
+                {
+                    exit = true;
+                    playerWon = true;
+                    //You have won
+                }
+
+
+            }
+
+            if (playerWon == true)
+            {
+                SpellItOut("Dealer: " + dealer.GetTotalValue() + ", Player: " + player.GetTotalValue(), timing);
+                Console.WriteLine("Player won");
+            }
+            else
+            {
+                SpellItOut("Dealer: " + dealer.GetTotalValue() + ", Player: " + player.GetTotalValue(), timing);
+                Console.WriteLine("Player lost");
+            }
+
+        }
+        //Methods
+
+        static int play(Cards cards)
+        {
+
+            SpellItOut(cards.GetName() + " receaved a " + cards.GiveNewCard(), 30);
+            cards.GetCurrentCardGraphic();
+            Thread.Sleep(2000);
+
+            return cards.GetCardValue();
+        }
+        static void Intro()
+        {
+            string line = "";
+            int timing = 50; //Standard timing
+
+            //Story
+            line = "Hello there, Creature!";
+            SpellItOut(line, timing);
+            Thread.Sleep(1000); //1 sek
+
+            line = "I am the dealer, and this is Black Jack";
+            SpellItOut(line, timing);
+            Thread.Sleep(1000);
+
+            line = "Have you played Black Jack before?";
+            timing = 30;
+            SpellItOut(line, timing);
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.WriteLine("Yes: 1, No: 2");
+            Console.ForegroundColor = ConsoleColor.White;
+
+            if (YesOrNo() == false)
+            {
+                Tutorial();
+            }
+
+            line = "Lets start";
+            SpellItOut(line, timing);
+        }
+        static void SpellItOut(string line, int timing)
+        {
+            string stringToFill = "";
+
+            for (int i = 0; i < line.Length; i++)
+            {
+                Console.Clear();
+                stringToFill += line[i];
+                Console.WriteLine(stringToFill);
+                Thread.Sleep(timing);
+            }
+        }
+        static bool YesOrNo()
+        {
+            int input = -1;
+            while (input == -1)
+            {
+                if (int.TryParse(Console.ReadLine(), out input) && input == 1)
+                {
+                    return true;
+                }
+                else if (input == 2)
+                {
+                    return false;
+                }
+                else
+                {
+                    Console.WriteLine("Choose a valid option");
+                    input = -1;
+                }
+            }
+            return false;
+        }
+        static void Tutorial()
+        {
+            Console.Clear();
+            Console.WriteLine("Tutorial:");
+            Console.WriteLine("You are playing against the dealer.");
+            Console.WriteLine("Each of you will get one card each for each round.");
+            Console.WriteLine("The goal is to get as close to the total sum of 21 as you can");
+            Console.WriteLine("Each round you get to choose if you want to get a new card to add to your sum");
+            Console.WriteLine("If your sum ends up bigger than 21, you loose");
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+    }
+}
